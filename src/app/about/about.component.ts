96fb534f86a4bcad87bcacc4642150af41c6044c
maiziheimei <wang.xia@campus.tu-berlin.de>
2017-11-20 18:35:18 +0100

    second commit

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,14 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  animations: [
+
+    trigger('goals', [
+      transition('* => *', [
+
+        query(':enter', style({ opacity: 0 }), {optional: true}),
+
+        query(':enter', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
+          ]))]), {optional: true})
+        ,
+        query(':leave', stagger('300ms', [
+          animate('.6s ease-out', keyframes([
+            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 0, transform: 'translateY(-75%)',     offset: 1.0}),
+          ]))]), {optional: true})
+      ])
+    ])
+
+  ]
 })
 export class AboutComponent implements OnInit {
+
+  goalText: string = 'My first life goal';
+  goals = ['first item', 'second item'];
 
   constructor() {}
 
   ngOnInit() {}
+
+  addItem(){
+    if(this.goalText.length >0)
+      this.goals.push(this.goalText);
+    this.goalText = '';
+  }
+
+  removeItem(i) {
+    this.goals.splice(i, 1);
+  }
 
 }
